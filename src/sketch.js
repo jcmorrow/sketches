e49793011fa983cfc00a86df340034d3c695feec
Josh Morrow <josh@jcmorrow.com>
2018-02-02 18:31:06 -0800

    Neat.

--- src/sketch.js
+++ src/sketch.js
@@ -16,49 +16,82 @@ const sketch = (p) => {
   }
 
   const gauss = (mean, variance) => (
-    mean + variance * p.randomGaussian()
+    Math.abs(mean + variance * p.randomGaussian())
   )
 
+  // p._draw = () => {
+  //   p.fill(100, 0, 0, .1);
+  //   _.range(20).map((i) => {
+
+  //     const topXExtend = Math.abs(gauss(0, 100));
+  //     const topWidth = this.minimumWidth + topXExtend;
+  //     const rightTopOffset = -1 * Math.abs(gauss(0.1 * topXExtend, 1.0))
+
+  //     const bottomXExtend = Math.abs(gauss(0, 100));
+  //     const bottomWidth = this.minimumWidth + bottomXExtend;
+  //     const rightBottomOffset = Math.abs(gauss(0.1 * bottomXExtend, 1.0))
+
+  //     p.beginShape();
+  //     p.vertex(
+  //       this.leftX,
+  //       this.topY,
+  //     );
+  //     p.vertex(
+  //       this.leftX + this.minimumWidth,
+  //       this.topY,
+  //     );
+  //     p.vertex(
+  //       this.leftX + topWidth,
+  //       this.topY + rightTopOffset,
+  //     );
+  //     p.vertex(
+  //       this.leftX + bottomWidth,
+  //       this.topY + rightTopOffset + this.rectHeight + rightBottomOffset,
+  //     );
+  //     p.vertex(
+  //       this.leftX + this.minimumWidth,
+  //       this.topY + this.rectHeight,
+  //     );
+  //     p.vertex(
+  //       this.leftX,
+  //       this.topY + this.rectHeight,
+  //     );
+  //     p.endShape(p.CLOSE);
+  //     p.noLoop();
+  //   })
+  // }
+
+  const left = (width) => (this.leftX + width);
+  const minWidth = (width) => (this.minimumWidth + width);
+  const top = (width) => (this.topY + width);
+
   p.draw = () => {
-    p.fill(75, 100, 100, .1);
+    p.fill(100, 0, 0, .1);
     _.range(20).map((i) => {
-
-      const topXExtend = Math.abs(gauss(0, 100));
-      const topWidth = this.minimumWidth + topXExtend;
-      const rightTopOffset = -1 * Math.abs(gauss(0.1 * topXExtend, 1.0))
-
-      const bottomXExtend = Math.abs(gauss(0, 100));
-      const bottomWidth = this.minimumWidth + bottomXExtend;
-      const rightBottomOffset = Math.abs(gauss(0.1 * bottomXExtend, 1.0))
+      const topWidth = minWidth(gauss(0, 20));
+      const control1Width = minWidth(40 + gauss(0, 100));
+      const control2Width = minWidth(40 + gauss(0, 100));
+      const bottomWidth = minWidth(gauss(0, 20));
 
       p.beginShape();
-      p.vertex(
-        this.leftX,
-        this.topY,
-      );
-      p.vertex(
-        this.leftX + this.minimumWidth,
-        this.topY,
-      );
-      p.vertex(
-        this.leftX + topWidth,
-        this.topY + rightTopOffset,
-      );
-      p.vertex(
-        this.leftX + bottomWidth,
-        this.topY + rightTopOffset + this.rectHeight + rightBottomOffset,
-      );
-      p.vertex(
-        this.leftX + this.minimumWidth,
-        this.topY + this.rectHeight,
-      );
-      p.vertex(
-        this.leftX,
-        this.topY + this.rectHeight,
+      p.vertex(left(0), top(0));
+      p.vertex(left(topWidth), top(0));
+      p.vertex(left(bottomWidth), top(this.rectHeight));
+      p.vertex(left(0), top(this.rectHeight));
+      p.endShape(p.CLOSE);
+
+      p.beginShape();
+      p.vertex(left(0), top(0));
+      p.bezier(
+        left(topWidth), top(0),
+        left(control1Width), top(this.rectHeight * 0.333),
+        left(control2Width), top(this.rectHeight * 0.666),
+        left(bottomWidth), top(this.rectHeight),
       );
       p.endShape(p.CLOSE);
-      p.noLoop();
+      return null;
     })
+    p.noLoop();
   }
 
   p.mousePressed = () => {
