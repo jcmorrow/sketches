e12a43048b90a962357b5bb996a30af87083a015
Josh Morrow <josh@jcmorrow.com>
2018-09-29 16:35:20 +0700

    This is good too

--- src/spinningPerlins.js
+++ src/spinningPerlins.js
@@ -1,44 +1,37 @@
 import _ from "underscore";
 
 const LINKS_COUNT = 1000;
-const SIZE = 800;
-const MAX_LENGTH = 20;
-const MAX_ANGLE = 3.1456;
+const MAX_LENGTH = 10;
 const RADIUS = 4;
+const SIZE = 800;
 const TIME_SCALE = 0.001;
-
-const randomAngle = () => Math.PI * Math.random() * 2.0;
-const randomGroup = () => (Math.random() > 0.5 ? 0 : 1);
 
 class Link {
   constructor(l) {
-    this.next = l;
+    this.angle = 0;
     this.length = Math.random() * MAX_LENGTH;
-  }
-
-  translate(p) {
-    window.p.translate(0, this.length);
+    this.next = l;
   }
 
   draw(p, time) {
-    window.p.ellipse(0, 0, RADIUS, RADIUS);
     if (this.next) {
-      window.p.push();
+      p.ellipse(0, 0, RADIUS, RADIUS);
+      p.push();
 
-      window.p.rotate(window.p.PI * window.p.noise(this.length, time));
+      this.angle = p.noise(this.length, time);
+      p.rotate(p.PI * this.angle);
 
-      window.p.line(0, RADIUS / 2, 0, this.next.length - RADIUS / 2);
-      this.next.translate(window.p);
-      this.next.draw(window.p, time);
-      window.p.pop();
+      p.line(0, RADIUS / 2, 0, this.next.length - RADIUS / 2);
+      p.translate(0, this.next.length);
+      this.next.draw(p, time);
+      p.pop();
     }
   }
 }
 
 const spinningPerlins = p => {
   p.setup = () => {
-    p.frameRate(10);
-    window.p = p;
+    p = p;
     const canvas = p.createCanvas(SIZE, SIZE);
     canvas.parent("sketch");
 
@@ -51,11 +44,9 @@ const spinningPerlins = p => {
 
   p.draw = () => {
     p.clear();
-    p.push();
+    p.stroke(0, 0, 0, 100);
     p.translate(SIZE / 2, SIZE / 2);
-    this.link.translate(p);
     this.link.draw(p, this.time);
-    p.pop();
     this.time = this.time + TIME_SCALE;
   };
 };
