6ceb08bf73973ef447c0f6171365cf4dab591149
Josh Morrow <josh@jcmorrow.com>
2018-10-16 16:22:21 +0900

    IDK

--- src/spinningPerlins.js
+++ src/spinningPerlins.js
@@ -1,28 +1,31 @@
 import _ from "underscore";
 
-const LINKS_COUNT = 1000;
-const MAX_LENGTH = 10;
-const RADIUS = 10;
-const SIZE = 800;
+const LINKS_COUNT = 100;
+const MAX_LENGTH = 20;
+const RADIUS = 20;
+const SIZE = 2000;
 const TIME_SCALE = 0.001;
 
 class Link {
   constructor(l) {
     this.length = Math.random() * MAX_LENGTH;
     this.next = l;
+    this.angle = 0;
   }
 
-  draw(p, time) {
+  draw(p, time, currentPoint) {
     if (this.next) {
-      const noise = p.noise(this.length, time);
-      p.ellipse(0, 0, this.length, this.length);
+      const noise = p.noise(this.length, time, this.angle * TIME_SCALE);
+      this.angle = noise / 2.0;
+      // p.ellipse(0, 0, this.length, this.length);
       p.push();
 
-      p.rotate(p.PI * noise);
+      p.rotate(p.PI * this.angle);
 
-      // p.line(0, RADIUS / 2, 0, this.next.length - RADIUS / 2);
+      p.stroke(255 * (1 - this.angle));
+      p.line(0, 0, 0, this.next.length);
       p.translate(0, this.next.length);
-      this.next.draw(p, time);
+      this.next.draw(p, time, currentPoint);
       p.pop();
     }
   }
@@ -30,7 +33,7 @@ class Link {
 
 const spinningPerlins = p => {
   p.setup = () => {
-    p = p;
+    window.p = p;
     const canvas = p.createCanvas(SIZE, SIZE);
     canvas.parent("sketch");
 
@@ -45,6 +48,7 @@ const spinningPerlins = p => {
     p.clear();
     // p.stroke(0, 0, 0, 25);
     // p.noStroke();
+    p.frameRate(50);
     p.fill(255, 255, 255);
     p.translate(SIZE / 2, SIZE / 2);
     this.link.draw(p, this.time);
