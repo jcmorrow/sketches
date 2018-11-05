99c5a3e2f15a7e230d394b203eac9d04c8540ef7
Josh Morrow <josh@jcmorrow.com>
2018-11-05 17:39:35 +0800

    This is the actual copy

--- src/sequence.js
+++ src/sequence.js
@@ -1,4 +1,4 @@
-const SIZE = 1000;
+const SIZE = 1500;
 
 const BEZIER_CONTROL_POINT_TANGENT_COEFFICIENT =
   (4.0 / 3.0) * (Math.sqrt(2) - 1);
@@ -15,24 +15,31 @@ const sequence = p => {
     this.cursor = 0;
     this.step = 1;
     this.visited = new Set();
+
+    p.noFill();
   };
 
   const halfCircle = (from, to) => {
-    console.log(from, to);
-    const radius = (to - from) * RADIUS;
-    console.log("Radius: ", radius);
+    const up = this.step % 2 === 0 ? -1 : 1;
+    const right = from > to ? -1 : 1;
+
+    const radius = Math.abs(to - from) * RADIUS;
+
     const startX = from * DIAMETER;
     const startY = 0;
-    const midX = startX + radius;
-    const midY = -1 * radius;
+
+    const midX = startX + radius * right;
+    const midY = radius * up;
+
     const endX = to * DIAMETER;
+    const endY = 0;
 
     p.bezier(
       startX,
-      0,
+      startY,
       startX,
       midY * BEZIER_CONTROL_POINT_TANGENT_COEFFICIENT,
-      midX - radius * BEZIER_CONTROL_POINT_TANGENT_COEFFICIENT,
+      midX + -1 * right * radius * BEZIER_CONTROL_POINT_TANGENT_COEFFICIENT,
       midY,
       midX,
       midY
@@ -40,40 +47,31 @@ const sequence = p => {
     p.bezier(
       midX,
       midY,
-      midX + radius * BEZIER_CONTROL_POINT_TANGENT_COEFFICIENT,
+      midX + right * radius * BEZIER_CONTROL_POINT_TANGENT_COEFFICIENT,
       midY,
       endX,
       midY * BEZIER_CONTROL_POINT_TANGENT_COEFFICIENT,
       endX,
-      0
+      endY
     );
   };
 
   p.draw = () => {
     p.translate(0, 500);
-    p.noFill();
+    this.visited.add(this.cursor);
 
-    p.line(0, 0, 1000, 0);
+    const back = this.cursor - this.step;
+    const forward = this.cursor + this.step;
+    let next;
 
-    for (let i = 0; i < 200; i++) {
-      this.visited.add(this.cursor);
-
-      const back = this.cursor - this.step;
-      const forward = this.cursor + this.step;
-      let next;
-
-      if (back > 0 && !this.visited.has(back)) {
-        next = back;
-      } else {
-        next = forward;
-      }
-      halfCircle(this.cursor, next);
-      this.cursor = next;
-      this.step++;
-      console.log(this.cursor);
+    if (back > 0 && !this.visited.has(back)) {
+      next = back;
+    } else {
+      next = forward;
     }
-
-    p.noLoop();
+    halfCircle(this.cursor, next);
+    this.cursor = next;
+    this.step++;
   };
 };
 
